a1762261ba6b65427b56647cb1e3ac643fe35c1f
Molina Thibaut <thibautmolina@yahoo.fr>
2020-01-23 10:27:09 +0100

    first commit

---
+++ sketch_dec22a/sketch_dec22a.ino
@@ -0,0 +1,51 @@
+
+#include <math.h>
+#include <dht11.h>
+
+
+#include <stdlib.h>
+#include <stdio.h>
+
+
+#define DHT11PIN A0 // broche dht11
+#define lm35Pin A2 // lm35
+#define fp=fopen("D:/Adruino","w");
+
+float tempdht; // On initialise la variable de température
+float humidtedht;
+int b=3975;
+dht11 DHT11;
+float tempLm35;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.println("DHT11  test!!");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  tempLm35=lm35dz();
+  Serial.print("Lum : ");
+  Serial.print(tempLm35);
+  Serial.print("\t");
+  DHT11.read(DHT11PIN);
+  File *fp;
+  fp=fopen("D:/Adruino","w");
+  fprintf(fp,"This is testing ... \n");
+  fclose(fp);
+  Serial.print("Humidité (%) : ");
+  Serial.print((float)DHT11.humidity,2);
+  Serial.print("\t");
+  Serial.print("Température (°C) : ");
+  Serial.println((float)DHT11.temperature,2); 
+  delay(2000); //un délai pour que le tout soit lisible
+}
+
+float lm35dz(){
+  float reading = analogRead(lm35Pin); //LM35DZ connect to Analog pin 3
+ //convert the voltage into temperature because the LM35DZ is at 10mV per °C
+ float voltage = reading * 5.0 / 1023.0;
+ float temperature = voltage*100.0;
+ return temperature;
+}
